7a76e879aa191b3a959637f7a4547dc5692f44c0
Guillaume Lazzara <guillaume.lazzara@pix.fr>
2022-03-21 18:22:05 +0100

    Declare /frameworks route
    Co-authored-by: Jérémie Jadé <jeremie.jade@pix.fr>
    #Co-authored-by: Khadija Bayssir <khadija.bayssir@pix.fr>
    Co-authored-by: Clarisse Damon <clarisse.damon@pix.fr>
    #Co-authored-by: François de Metz <francois.de-metz@pix.fr>
    #Co-authored-by: Thomas Evano <thomas.evano@pix.fr>
    #Co-authored-by: Nicolas Legrand <nicolas.legrand@pix.fr>
    #Co-authored-by: Vincent Hardouin <vincent.hardouin@pix.fr>

--- api/lib/application/frameworks/index.js
+++ api/lib/application/frameworks/index.js
@@ -1,5 +1,7 @@
+const Joi = require('joi');
 const frameworkController = require('./frameworks-controller');
 const securityPreHandlers = require('../security-pre-handlers');
+const identifiersType = require('../../domain/types/identifiers-type');
 
 exports.register = async function (server) {
   server.route([
@@ -25,7 +27,25 @@ exports.register = async function (server) {
         tags: ['api', 'framework'],
         notes: [
           "Cette route est restreinte aux utilisateurs authentifiés membre d'une organisation",
-          "Elle permet de demander de récupérer toutes les données du référentiel Pix jusqu'aux sujets",
+          "Elle permet de récupérer toutes les données du référentiel Pix jusqu'aux sujets",
+        ],
+      },
+    },
+    {
+      method: 'GET',
+      path: '/api/frameworks/{id}',
+      config: {
+        handler: frameworkController.getFramework,
+        pre: [{ method: securityPreHandlers.checkUserHasRolePixMaster }],
+        validate: {
+          params: Joi.object({
+            id: identifiersType.frameworkId,
+          }),
+        },
+        tags: ['api', 'framework'],
+        notes: [
+          "Cette route est restreinte aux utilisateurs authentifiés membre d'une organisation",
+          "Elle permet de récupérer toutes les données d'un référentiel jusqu'aux sujets",
         ],
       },
     },
